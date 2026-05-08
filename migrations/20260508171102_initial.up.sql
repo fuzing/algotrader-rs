c842a1159d80807f4cb13b3409f97982c6f179e6
Peter Beresford <peter@fuzing.com>
2026-05-08 11:32:09 -0600

    ok

--- migrations/20260508171102_initial.up.sql
+++ migrations/20260508171102_initial.up.sql
@@ -1,2 +1,7 @@
 -- Add up migration script here
-CREATE TABLE users (id INT PRIMARY KEY);
+CREATE TABLE IF NOT EXISTS users (
+     id SERIAL PRIMARY KEY,
+     username TEXT NOT NULL UNIQUE,
+     created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+);
+
