01006120b3d899836ff17c1084f03d5ba37dad5e
Peter Beresford <peter@fuzing.com>
2026-05-08 11:54:30 -0600

    ok

--- migrations/20260508171102_initial.down.sql
+++ migrations/20260508171102_initial.down.sql
@@ -1,2 +1,8 @@
 -- Add down migration script here
+
+
+DROP TABLE IF EXISTS equities;
+DROP TABLE IF EXISTS equity_prices;
+DROP TABLE IF EXISTS option_contracts;
+DROP TABLE IF EXISTS option_contract_prices;
 DROP TABLE IF EXISTS users;
