b3c62f3c1fccfd04ee6b4e6dcd4dc9618271d719
Peter Beresford <peter@fuzing.com>
2026-05-28 10:53:44 -0600

    ok

--- bin/test/src/main.rs
+++ bin/test/src/main.rs
@@ -1,26 +1,61 @@
 
 
-const PATCH_TEMPORAL_WINDOW_SIZE: usize = 5;
-const PATCH_TEMPORAL_STRIDE: usize = 1;
-const LOB_LEVELS: usize = 1;
-
-const PREDICTION_TEMPORAL_WINDOW_SIZE: usize = 10;
+const NUMBER_OF_SNAPSHOTS: usize = 199;
+const PREDICTION_TEMPORAL_WINDOW_SIZE: usize = 100;
 
 
-const NUMBER_OF_SNAPSHOTS: usize = 1_000;
+#[derive(Debug)]
+pub struct Level {
+    price: f64,
+    volume: u32,
+}
+
+#[derive(Debug)]
+pub struct Snapshot {
+    bids: Vec<Level>,
+    asks: Vec<Level>,
+}
+
+
+#[derive(Debug)]
+pub struct PriceGainItem {
+    pub patches: PriceGainPatches,
+    pub label: f64,
+}
+
+
+#[derive(Debug)]
+pub struct PriceGainDataset {
+    items: Vec<PriceGainItem>,           // the read in data file
+}
+
+const PATCH_TEMPORAL_WINDOW_SIZE: usize = 4;
+const PATCH_TEMPORAL_STRIDE: usize = PATCH_TEMPORAL_WINDOW_SIZE;
+const LOB_LEVELS: usize = 10;
 
 
 type PatchData = [[f64; LOB_LEVELS]; PATCH_TEMPORAL_WINDOW_SIZE];
 
 
-struct Level {
-    price: f64,
-    volume: u32,
+#[derive(Debug)]
+pub struct PriceGainPatch {
+    pub data: Box<PatchData>,
 }
 
-struct Snapshot {
-    bids: Vec<Level>,
-    asks: Vec<Level>,
+impl PriceGainPatch {
+    pub fn new(data: PatchData) -> Self {
+        Self {
+            data: Box::new(data),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct PriceGainPatches {
+    ask_price: Vec<PriceGainPatch>,
+    bid_price: Vec<PriceGainPatch>,
+    ask_volume: Vec<PriceGainPatch>,
+    bid_volume: Vec<PriceGainPatch>,
 }
 
 
@@ -42,9 +77,17 @@ fn main() {
     let volume_mean: f64 = 10.0;
     let volume_std_dev: f64 = 2.0;
 
-    let mut patches: Vec<PatchData> = Vec::new();
+    let mut items: Vec<PriceGainItem> = Vec::new();
 
     for i in 0..=(snapshots.len() - PREDICTION_TEMPORAL_WINDOW_SIZE) {
+        let mut patches = PriceGainPatches {
+            bid_price: Vec::new(),
+            bid_volume: Vec::new(),
+            ask_price: Vec::new(),
+            ask_volume: Vec::new(),
+        };
+
+
         for j in (0..=(PREDICTION_TEMPORAL_WINDOW_SIZE - PATCH_TEMPORAL_WINDOW_SIZE)).step_by(PATCH_TEMPORAL_STRIDE) {
             let mut bid_price_patch: PatchData = [[0.0; LOB_LEVELS]; PATCH_TEMPORAL_WINDOW_SIZE];
             let mut ask_price_patch: PatchData = [[0.0; LOB_LEVELS]; PATCH_TEMPORAL_WINDOW_SIZE];
@@ -60,17 +103,29 @@ fn main() {
                 }
             }
 
-            patches.push(bid_price_patch);
-            patches.push(bid_volume_patch);
-            patches.push(ask_price_patch);
-            patches.push(ask_volume_patch);
+            // add patches
+            patches.bid_price.push(PriceGainPatch::new(bid_price_patch));
+            patches.bid_volume.push(PriceGainPatch::new(bid_volume_patch));
+            patches.ask_price.push(PriceGainPatch::new(ask_price_patch));
+            patches.ask_volume.push(PriceGainPatch::new(ask_volume_patch));
         }
+
+        // let label = data_file.data[i].trade_gain;
+        let label = 23.0;
+
+        items.push(
+            PriceGainItem {
+                patches,
+                label,
+            }
+        );
+
+
     }
 
     println!("Total number of snapshots: {}", snapshots.len());
-    println!("Total number of patches: {}", patches.len());
-
-
+    println!("Total number of items: {}", items.len());
+    println!("Total number of patches per item: {}", items[0].patches.ask_price.len() * 4);
 }
 
 
